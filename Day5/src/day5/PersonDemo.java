7870ecb0c693ba81c1e85bdcc3b2d4f60dac3ee6
SiXinhong <879318555@qq.com>
2017-06-06 21:55:29 +0800

    day05

---
+++ Day5/src/day5/PersonDemo.java
@@ -0,0 +1,33 @@
+package day5;
+
+class Person
+{
+	private int age;
+	public void setAge(int a)
+	{
+		if (a > 0&&a < 130){
+			age = a;
+		}
+		else{
+			System.out.println("²»ºÏ·¨");
+		}
+	}
+	public int getAge()
+	{
+		return age;
+	}
+	void speak()
+	{
+		System.out.println("age="+age);
+	}
+}
+public class PersonDemo {
+	public static void main(String[] args){
+		Person p = new Person();
+		//p.age = 20;
+		p.setAge(20);
+		p.speak();
+	
+	}
+	
+}
