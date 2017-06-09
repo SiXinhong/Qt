f1643b300bff1605cb43d9b8f8a4f185f16ea24b
SiXinHong <879318555@qq.com>
2017-06-09 14:38:49 +0800

    修改弹出式菜单。

--- qjwidget.h
+++ qjwidget.h
@@ -14,6 +14,7 @@ private:
 //Ui::Widget *ui;
 QAction* To_Zhu;
 QAction* To_Ningshi;
+QAction* To_Tanchu;
 
 public:
     explicit QjWidget(QWidget *parent = 0);
@@ -21,13 +22,19 @@ public:
 signals:
 
 public slots:
+    //到主显示区显示菜单处理事件
     void ToZhu()
     {
         qDebug()<<"到主显示区。";
     }
+    //到凝视显示区显示菜单处理事件
     void ToNingshi()
     {
         qDebug()<<"到凝视显示区。";
+    }
+    //到弹出窗口显示菜单处理事件
+    void ToTanchu(){
+        qDebug()<<"到弹出窗口。";
     }
 };
 
